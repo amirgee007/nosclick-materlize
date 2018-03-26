d811d45ee7b4dbe3471bc4d530fbdc7f25f4e709
Amir SeerSol <amirgee007@yahoo.com>
2018-03-26 20:43:24 +0500

    remove buttons

--- cronlab/resources/views/user/deposit/create.blade.php
+++ cronlab/resources/views/user/deposit/create.blade.php
@@ -22,6 +22,7 @@
                                         <i class="material-icons">flash_on</i>Stripe
                                     </a>
                                 </li>
+
                                 <li class="">
                                     <a href="#instant_payeer" role="tab" data-toggle="tab">
                                         <i class="material-icons">flash_on</i>Payeer
@@ -65,25 +66,15 @@
                                             <br>
                                         @endif
 
-                                        <div class="row">
-                                            <div class="col-md-6 col-md-offset-1">
-                                                <div class="form-group label-floating">
-                                                    <select class="selectpicker" name="gateway" data-style="btn btn-warning btn-round" title="Processeur de paiement" data-size="7">
-                                                        @foreach($gateways as $gateway)
-                                                            @if($gateway->name=='Stripe')
-                                                            <option selected value="{{$gateway->id}}">{{$gateway->name}}</option>
-                                                            @endif
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-                                            </div>
-
-                                        </div>
+                                        @foreach($gateways as $gateway)
+                                            @if($gateway->name=='Stripe')
+                                                <input type="hidden" name="gateway" value="{{$gateway->id}}">
+                                            @endif
+                                        @endforeach
                                         <br>
                                         <div class="row">
 
                                             <div class="col-md-6 col-md-offset-1">
-
                                                 <div class="form-group label-floating">
                                                     <label  class="control-label" for="amount">Montant du dépôt</label>
                                                     <input id="amount" name="amount" type="text" class="form-control">
@@ -99,7 +90,9 @@
 
                                 </div>
 
-                                <div class="tab-pane active" id="instant_payeer">
+
+
+                                <div class="tab-pane" id="instant_payeer">
                                     <div class="alert alert-info alert-with-icon">
                                         <i class="material-icons" data-notify="icon">notifications</i>
                                         <!--<span class="text-left">Les frais de transaction sont les suivants :</span><br>-->
@@ -108,7 +101,6 @@
                                                 <span><div class="card" style="width:40%" ><img src="{{$gateway->image}}"/></div> <br>  {{$gateway->id}}. <b>{{$gateway->name}}</b> vous facture <b>€ {{$gateway->fixed}}</b> fixe + <b>{{$gateway->percent}}%</b></span>
                                             @endif
                                         @endforeach
-
                                     </div>
 
 
@@ -126,20 +118,12 @@
                                             <br>
                                         @endif
 
-                                        <div class="row">
-                                            <div class="col-md-6 col-md-offset-1">
-                                                <div class="form-group label-floating">
-                                                    <select class="selectpicker" name="gateway" data-style="btn btn-warning btn-round" title="Processeur de paiement" data-size="7">
-                                                        @foreach($gateways as $gateway)
-                                                            @if($gateway->name=='Payeer')
-                                                                <option selected value="{{$gateway->id}}">{{$gateway->name}}</option>
-                                                            @endif
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-                                            </div>
+                                        @foreach($gateways as $gateway)
+                                            @if($gateway->name=='Payeer')
+                                                <input type="hidden" name="gateway" value="{{$gateway->id}}">
+                                            @endif
+                                        @endforeach
 
-                                        </div>
                                         <br>
                                         <div class="row">
                                             <div class="col-md-6 col-md-offset-1">
@@ -150,15 +134,14 @@
                                             </div>
                                         </div>
                                         <br><br>
-                                        <button type="submit" class="btn btn-success pull-right">Valider</button>
+
+                                        <button type="submit" class="btn btn-success pull-right">Pay with Payeer</button>
                                         <a href="{{route('userDeposits')}}" class="btn btn-rose pull-right">Annuler</a>
                                         <div class="clearfix"></div>
                                     </form>
 
 
                                 </div>
-
-
 
                                 <div class="tab-pane" id="local">
                                     <div class="alert alert-info alert-with-icon">
