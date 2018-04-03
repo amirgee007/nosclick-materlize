4f212f2682a7b6db02adebbe55eb0496bc26c88a
Amir SeerSol <amirgee007@yahoo.com>
2018-04-03 18:12:50 +0500

    update desing

--- cronlab/resources/views/user/myreferral.blade.php
+++ cronlab/resources/views/user/myreferral.blade.php
@@ -3,140 +3,134 @@
 @section('content')
 
 
-<script>
-function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  document.execCommand("Copy");
-  alert("Votre lien: " + copyText.value);
-}
-</script>
+    <script>
+        function myFunction() {
+            var copyText = document.getElementById("myInput");
+            copyText.select();
+            document.execCommand("Copy");
+            alert("Votre lien: " + copyText.value);
+        }
+    </script>
 
 
     <div class="row">
         <div class="col-md-12">
-             <div class="table-responsive">
-            <div class="card">
-                <div class="card-header card-header-icon" data-background-color="green">
-                    <i class="material-icons">supervisor_account</i>
-                </div>
-                <br>
-                <h4 class="card-title">Mes parrainages</h4>
-                <div class="card-content">
+            <div class="table-responsive">
+                <div class="card">
+                    <div class="card-header card-header-icon" data-background-color="green">
+                        <i class="material-icons">supervisor_account</i>
+                    </div>
                     <br>
+                    <h4 class="card-title">Mes parrainages</h4>
+                    <div class="card-content">
+                        <br>
+                        <div class="table-responsive">
+                            <br>
+                            <div class="col-md-12">
+
+
+                                @if($sponsor)
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th class="text-center">SN</th>
+                                            <th class="text-center">Photo</th>
+                                            <th class="text-center">Nom</th>
+                                            <th class="text-center">Abonnement</th>
+                                            <th class="text-center">Statut</th>
+                                            <th class="text-center">Inscription</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td class="text-center">1</td>
+                                            <td class="text-center" width="10%" >
+                                                <img src="{{@$sponsor->profile->avatar}}" class="img-circle" alt="No Photo"  >
+                                            </td>
+                                            <td class="text-center">{{@$sponsor->name}}</td>
+                                            <td class="text-center">{{@$sponsor->membership->name}}</td>
+                                            <td class="text-center">Actif</td>
+                                            <td class="text-center">{{@$sponsor->created_at->diffForHumans()}}</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                @endif
+
+
+                                <div class="row">
+                                    <div class="col-md-12"
+
+                                    <h4><div class="card card-title pull-left text-center">
+
+                                            <input class="btn btn-info" style="width:60%" type="text" value="{{ $link }}" id="myInput">
+                                            <div class="btn" onclick="myFunction()">Copier votre lien</div>
+                                        </div></h4>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <br>
+
                     <div class="table-responsive">
                         <br>
                         <div class="col-md-12">
 
-
-                    @if($sponsor)
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th class="text-center">SN</th>
-                                <th class="text-center">Photo</th>
-                                <th class="text-center">Nom</th>
-                                <th class="text-center">Abonnement</th>
-                                <th class="text-center">Statut</th>
-                                <th class="text-center">Inscription</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td class="text-center">1</td>
-                                <td class="text-center" width="10%" >
-                                    <img src="{{@$sponsor->profile->avatar}}" class="img-circle" alt="No Photo"  >
-                                </td>
-                                <td class="text-center">{{@$sponsor->name}}</td>
-                                <td class="text-center">{{@$sponsor->membership->name}}</td>
-                                <td class="text-center">Actif</td>
-                                <td class="text-center">{{@$sponsor->created_at->diffForHumans()}}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    @endif
-
-
-                        <div class="row">
-                         <div class="col-md-12"
-
-                         <h4><div class="card card-title pull-left text-center">
-
-                        <input class="btn btn-info" style="width:60%" type="text" value="{{ $link }}" id="myInput">
-                            <div class="btn" onclick="myFunction()">Copier votre lien</div>
-                            </div></h4>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            </div>
-                            </div>
-                             </div>
-                            </div>
-
-
-
-                        <br>
-
-                      <div class="table-responsive">
-                        <br>
-                        <div class="col-md-12">
-							
-							<br><br>
-                        @if(count($referrals) > 0)
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th class="text-center">SN</th>
-                                <th class="text-center">Photo</th>
-                                <th class="text-center">Nom</th>
-                                <th class="text-center">Abonnement</th>
-                                <th class="text-center">Statut</th>
-                                <th class="text-center">Inscription</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-
-                                @php $id=0;@endphp
-                                @foreach($referrals as $referral)
-                                    @php $id++;@endphp
+                            <br><br>
+                            @if(count($referrals) > 0)
+                                <table class="table">
+                                    <thead>
                                     <tr>
-                                        <td class="text-center">{{ $id }}</td>
-                                        <td class="text-center" width="10%" >
-                                            <img src="{{$referral->user->profile->avatar}}" class="img-circle" alt="No Photo"  >
-                                        </td>
-                                        <td class="text-center">{{$referral->user->name}}</td>
-                                        <td class="text-center">{{$referral->user->membership->name}}</td>
-                                        <td class="text-center">Actif</td>
-                                        <td class="text-center">{{$referral->user->created_at->diffForHumans()}}</td>
+                                        <th class="text-center">SN</th>
+                                        <th class="text-center">Photo</th>
+                                        <th class="text-center">Nom</th>
+                                        <th class="text-center">Abonnement</th>
+                                        <th class="text-center">Statut</th>
+                                        <th class="text-center">Inscription</th>
                                     </tr>
+                                    </thead>
+                                    <tbody>
 
-                                @endforeach
+
+                                    @php $id=0;@endphp
+                                    @foreach($referrals as $referral)
+                                        @php $id++;@endphp
+                                        <tr>
+                                            <td class="text-center">{{ $id }}</td>
+                                            <td class="text-center" width="10%" >
+                                                <img src="{{$referral->user->profile->avatar}}" class="img-circle" alt="No Photo"  >
+                                            </td>
+                                            <td class="text-center">{{$referral->user->name}}</td>
+                                            <td class="text-center">{{$referral->user->membership->name}}</td>
+                                            <td class="text-center">Actif</td>
+                                            <td class="text-center">{{$referral->user->created_at->diffForHumans()}}</td>
+                                        </tr>
+
+                                    @endforeach
 
 
-                            </tbody>
-                        </table>
+                                    </tbody>
+                                </table>
 
                             @else
-                           <center> <h1> Il n'y a aucun parrainage.</h1></center>
+                                <center> <h1> Il n'y a aucun parrainage.</h1></center>
 
-                        @endif
+                            @endif
                         </div>
-                        </div>
-            </div>
+                    </div>
                 </div>
             </div>
-
-        <div class="row">
-            <div class="col-sm-12 col-sm-offset-4">
+            <div class="col-sm-offset-4">
                 {{$referrals->render()}}
             </div>
         </div>
-
-            </div>
-		</div>
-
-
+    </div>
+    </div>
 
 @endsection
